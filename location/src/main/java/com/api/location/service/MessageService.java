93c0b39fda76ed46c8561fbd62a83171e871d587
jess <jess@jess-okami>
2024-10-17 11:50:39 +0200

    messages

--- location/src/main/java/com/api/location/service/MessageService.java
+++ location/src/main/java/com/api/location/service/MessageService.java
@@ -5,7 +5,10 @@ import com.api.location.model.Message;
 import com.api.location.model.MessageDTO;
 import com.api.location.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -19,6 +22,17 @@ public class MessageService {
   private final MessageMapper messageMapper;
 
   public ResponseEntity<?> addMessage(MessageDTO messageDTO) {
+    // Vérifie l'authentification de l'utilisateur
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || !authentication.isAuthenticated()) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // Retourne 401
+    }
+
+    // Vérification de la validité du message
+    if (messageDTO.getMessage() == null || messageDTO.getMessage().isEmpty()) {
+      return ResponseEntity.badRequest().build(); // Retourne 400
+    }
+
     Message message = messageMapper.messageDTOToMessage(messageDTO);
     repository.save(message);
 
